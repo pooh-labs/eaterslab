6821b1ce078cd7adf2202c90ca921642bfef8dc7
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-07 23:39:34 +0100

    static files fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -117,4 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+os.makedirs(STATIC_ROOT, exist_ok=True)
