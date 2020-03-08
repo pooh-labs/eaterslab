24e114bbb2914470a043db735205a898a5916282
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-08 10:35:33 +0100

    Revert "ref name fix"
    This reverts commit dc3b9ea20604206a7705d3497d9958020f53d51e.

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=sqc-v0#nkjz!cgggyb-n2coxc0_yahmjwoj04zv#ut_v=nka2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
