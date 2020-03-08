5a9e80bb3979e13721dab26dcd9ea5314b923df3
Robert Michna <robertos1727@gmail.com>
2020-03-08 16:09:24 +0100

    Updated gitignore and removed comment

--- server/home/views.py
+++ server/home/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 
 def index(request):
     return HttpResponse("Welcome to ivory beam made by killer geckos.")
