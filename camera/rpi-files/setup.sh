9bb2efdc825db23aa99f0916592a657d369ca5f5
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-03-17 21:18:18 +0100

    Camera: Set up Python

--- camera/rpi-files/setup.sh
+++ camera/rpi-files/setup.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# Force running from the same directory
+if [ "$PWD" != "${SCRIPT_PATH}" ]; then
+    echo >&2 "Error: Script called from a different directory" 
+    print_usage_and_die
+fi
+
 # Enable and start SSH
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+# Install Python
+sudo apt-get update
+sudo apt-get install python3.7
+
+# Set up virtual environment
+cd rpi-files
+python3 -m venv env
+
+# Install requirements
+source env/bin/activate
+pip install -r requirements.txt
+deactivate
