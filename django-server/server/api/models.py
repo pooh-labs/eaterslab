9837dbde30612566ab80a63181f9e9c9ce4f76af
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-22 21:40:23 +0100

    temp random migrations added

--- django-server/server/api/models.py
+++ django-server/server/api/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from random import random
 
 
 class Cafeteria(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=256)
     sub_description = models.CharField(max_length=512)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    capacity = models.IntegerField()
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
+    capacity = models.IntegerField(default=30 * random())
 
     def __str__(self):
         return self.name
