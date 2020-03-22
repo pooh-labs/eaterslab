94d01a83a4f95de7b6d7738f71363953f46d6530
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-23 00:21:49 +0100

    api geenration added

--- django-server/server/server/settings.py
+++ django-server/server/server/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',                                             # for API documentation and generation
     'api.apps.ApiConfig',
 ]
 
