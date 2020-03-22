c3148fbf9db040f6720f082a8f9d11e5b92a09c7
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-22 21:41:04 +0100

    remove default values

--- django-server/server/api/models.py
+++ django-server/server/api/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from random import random
 
 
 class Cafeteria(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=256)
     sub_description = models.CharField(max_length=512)
-    latitude = models.FloatField(default=0.0)
-    longitude = models.FloatField(default=0.0)
-    capacity = models.IntegerField(default=30 * random())
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    capacity = models.IntegerField()
 
     def __str__(self):
         return self.name
