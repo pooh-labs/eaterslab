a6752a475a85a800d2094500eec52a4c8ff96223
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-22 15:48:15 +0100

    action update with static files

--- django-server/server/server/wsgi.py
+++ django-server/server/server/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
