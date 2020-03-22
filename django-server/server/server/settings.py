b789555b3e23972e5c28a3cc6f22a995bc5f0a8f
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-22 14:26:19 +0100

    action update with static files

--- django-server/server/server/settings.py
+++ django-server/server/server/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MIDDLEWARE_CLASSES = [
     'whitenoise.middleware.WhiteNoiseMiddleware', # for static files on heroku management
 ]
 
