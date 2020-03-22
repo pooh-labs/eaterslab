16e44196d2de6ec257ea781c7f484127f16b1c72
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-22 21:32:01 +0100

    added latitude and longtitude to cafeteria model

--- django-server/server/api/models.py
+++ django-server/server/api/models.py
@@ -5,6 +5,9 @@ class Cafeteria(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=256)
     sub_description = models.CharField(max_length=512)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    capacity = models.IntegerField()
 
     def __str__(self):
         return self.name
--- django-server/server/api/serializers.py
+++ django-server/server/api/serializers.py
@@ -6,4 +6,4 @@ from .models import Cafeteria
 class CafeteriaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cafeteria
-        fields = ['id', 'name', 'description', 'sub_description']
+        fields = ['id', 'name', 'description', 'sub_description', 'longitude', 'latitude']
