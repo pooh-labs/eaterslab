4325da9f6d899e23f30b4bf650d4da6c62de243c
Maciej Procyk <macieekprocyk@gmail.com>
2020-03-24 15:12:48 +0100

    server setup chcanged to use pipenv

--- django-server/server-env-setup.sh
+++ django-server/server-env-setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# This script assumes you have python3, pip and virtualenv installed
-# (Sometimes you have to apt-get install python3-venv)
-# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
-echo "Starting installation"
-python3 -m venv serverenv
-source serverenv/bin/activate
-pip install django
-pip install django-environ
-pip install djangorestframework
+# This script assumes you have python3, pip and pipenv installed
+# you can install pipenv using pip3 install pipenv
+echo "Starting installation of virtual environment"
+cd server
+pipenv install
+pipenv shell
 echo "Done"
 
