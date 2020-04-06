b7edf07907f58d6afb1ffb3d0e6560dd05e78d02
Maciej Procyk <macieekprocyk@gmail.com>
2020-04-06 12:02:13 +0200

    added connection checker

--- android/app/src/main/java/labs/pooh/eaterslab/LoadingConnectionActivity.kt
+++ android/app/src/main/java/labs/pooh/eaterslab/LoadingConnectionActivity.kt
@@ -2,11 +2,27 @@ package labs.pooh.eaterslab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class LoadingConnectionActivity : AppCompatActivity() {
+    companion object {
+        private var isLoadingScreenActive = false
+
+        fun isLoadingScreenActive() = isLoadingScreenActive
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading_connection)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        isLoadingScreenActive = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        isLoadingScreenActive = false
     }
 }
