cf91a11fde0574f6a2e0c252b20d97c3f8a70c32
Maciej Procyk <macieekprocyk@gmail.com>
2020-04-21 23:43:39 +0200

    ahndle no file error

--- django-server/server/home/views.py
+++ django-server/server/home/views.py
@@ -10,10 +10,17 @@ def index(request):
 
 
 def artifact_response(response):
-    file = FileSystemStorage().open(path_join(settings.ARTIFACTS_ROOT, settings.ARTIFACT_NAME), 'rb')
-    return FileResponse(file)
+    return existing__binary_file_response(path_join(settings.ARTIFACTS_ROOT, settings.ARTIFACT_NAME))
 
 
 def artifact_beta_response(response):
-    file = FileSystemStorage().open(path_join(settings.ARTIFACTS_ROOT_BETA, settings.ARTIFACT_NAME), 'rb')
-    return FileResponse(file)
+    return existing__binary_file_response(path_join(settings.ARTIFACTS_ROOT_BETA, settings.ARTIFACT_NAME))
+
+
+def existing__binary_file_response(response, path):
+    try:
+        file = FileSystemStorage().open(path, 'rb')
+        return FileResponse(file, as_attachment=True)
+    except FileNotFoundError:
+        return HttpResponse("No file on server")
+
