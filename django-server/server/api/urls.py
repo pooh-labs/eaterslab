3aa5f647cbc4dff9555cb19d4a371c8b8e233b5c
Maciej Procyk <macieekprocyk@gmail.com>
2020-04-29 11:01:06 +0200

    name changes

--- django-server/server/api/urls.py
+++ django-server/server/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'cafeteria', views.CafeteriaViewSet)
+router.register(r'cafeterias', views.CafeteriaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
