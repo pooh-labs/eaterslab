76fd3a874ddb5e71519aefd5fe8ff24638aec473
Maciej Procyk <macieekprocyk@gmail.com>
2020-05-02 11:38:31 +0200

    extended base models

--- django-server/server/api/models.py
+++ django-server/server/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, URLValidator
 
 
 class Cafeteria(models.Model):
@@ -9,6 +9,29 @@ class Cafeteria(models.Model):
     latitude = models.FloatField(validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)])
     longitude = models.FloatField(validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)])
     capacity = models.IntegerField(validators=[MinValueValidator(1)])
+    logo_url = models.CharField(max_length=2048, validators=[URLValidator])
+    address = models.CharField(max_length=256)
+    opened_from = models.TimeField()
+    opened_to = models.TimeField()
 
     def __str__(self):
         return self.name
+
+
+class FixedMenuOption(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(validators=[MinValueValidator(0.0)])
+    cafeteria = models.ForeignKey(Cafeteria, on_delete=models.CASCADE)
+    photo_url = models.CharField(max_length=2048, validators=[URLValidator])
+
+
+class FixedMenuTag(models.Model):
+    name = models.CharField(max_length=32, editable=False)
+    option = models.ManyToManyField(FixedMenuOption)
+
+
+class FixedMenuOptionReview(models.Model):
+    option = models.ForeignKey(Cafeteria, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    author_nick = models.CharField(max_length=64)
+    review_time = models.DateTimeField(editable=False)
