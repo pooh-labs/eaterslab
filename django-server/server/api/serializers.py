7a8ae11f38899a62687dcdc67d0b0d5fa460c958
Maciej Procyk <macieekprocyk@gmail.com>
2020-05-05 00:29:55 +0200

    server cafeteria model (#97)
    * extended base models
    
    * added serializers
    
    * models adding
    
    * added android tests
    
    * added android tests
    
    * view sets and serializers defiend for get and put methods
    
    * migrations add to repo
    
    * fixed keys naming
    
    * sample env fixed
    
    * sample env fixed

--- django-server/server/api/serializers.py
+++ django-server/server/api/serializers.py
@@ -1,9 +1,35 @@
 from rest_framework import serializers
 
-from .models import Cafeteria
+from .models import Cafeteria, FixedMenuOption, FixedMenuOptionReview, MenuOptionTag
 
 
-class CafeteriaSerializer(serializers.HyperlinkedModelSerializer):
+class FixedMenuOptionReviewSerializer(serializers.ModelSerializer):
+    option = serializers.PrimaryKeyRelatedField(queryset=FixedMenuOption.objects.all(), many=False)
+
+    class Meta:
+        model = FixedMenuOptionReview
+        fields = ['stars', 'author_nick', 'review_time', 'option']
+
+
+class MenuOptionTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuOptionTag
+        fields = ['name']
+
+
+class FixedMenuOptionSerializer(serializers.ModelSerializer):
+    menu_option_tags = MenuOptionTagSerializer(many=True, read_only=True)
+    fixed_menu_option_reviews = FixedMenuOptionReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = FixedMenuOption
+        fields = ['name', 'price', 'photo_url', 'menu_option_tags', 'fixed_menu_option_reviews']
+
+
+class CafeteriaSerializer(serializers.ModelSerializer):
+    fixed_menu_options = FixedMenuOptionSerializer(many=True, read_only=True)
+
     class Meta:
         model = Cafeteria
-        fields = ['id', 'name', 'description', 'sub_description', 'longitude', 'latitude', 'capacity']
+        fields = ['id', 'name', 'description', 'sub_description', 'longitude', 'latitude',
+                  'logo_url', 'address', 'opened_from', 'opened_to', 'fixed_menu_options']
