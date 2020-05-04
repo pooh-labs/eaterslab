2e1227555f6136f8049728c29d80f2323e5256df
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-05-04 12:16:32 +0200

    Implement FrameIngestor (#95)
    * Implement base FrameIngestorSource
    * Add FrameIngestor unit tests
    * Add FileIngestorSource

--- camera/rpi-files/format-lint-test.sh
+++ camera/rpi-files/format-lint-test.sh
@@ -9,17 +9,20 @@ function join {
 
 SOURCES=(
     "main.py" \
-    "people_counter.py" \
     "data_batcher.py" \
+    "frame_ingestor.py" \
+    "people_counter.py"
 )
 
 TESTS=(
-    "test_data_batcher.py"
+    "test_data_batcher.py" \
+    "test_frame_ingestor.py"
 )
 
 ALL_FILES=$(IFS=$' '; echo "${SOURCES[*]} ${TESTS[*]}")
+ALL_TESTS=$(IFS=$' '; echo "${TESTS[*]}")
 
 isort ${ALL_FILES} &&
 black -t py37 -S -l 79 ${ALL_FILES} &&
-flake8 --config=setup.cfg ${ALL_FILES} &&
-pytest ${TESTS}
+flake8 --config=setup.cfg ${ALL_FILES}
+pytest ${ALL_TESTS}
--- camera/rpi-files/main.py
+++ camera/rpi-files/main.py
@@ -11,9 +11,15 @@ import os
 import signal
 import time
 
+import cv2
 import openapi_client
 from data_batcher import DataBatcher
 from dotenv import load_dotenv
+from frame_ingestor import (
+    FileIngestorSource,
+    FrameIngestor,
+    WebcamIngestorSource,
+)
 from people_counter import PeopleCounter
 
 BATCH_SECONDS = 3
@@ -24,13 +30,14 @@ class Main(object):
 
     def __init__(self):
         """Initialize signal handlers, API client and run."""
-        self.should_close = False
-        self.counter = PeopleCounter()
-        self.batcher = DataBatcher()
+        self._should_close = False
+        self._counter = PeopleCounter()
+        self._frame_ingestor = FrameIngestor()
+        self._batcher = DataBatcher()
 
         # Add signal handlers
-        signal.signal(signal.SIGINT, self.handle_signals)
-        signal.signal(signal.SIGTERM, self.handle_signals)
+        signal.signal(signal.SIGINT, self._handle_signals)
+        signal.signal(signal.SIGTERM, self._handle_signals)
 
         # Set API configuration
         configuration = openapi_client.Configuration()
@@ -38,51 +45,117 @@ class Main(object):
         configuration.api_key['X-DEVICE-ID'] = os.getenv('DEVICE_ID')
         configuration.api_key['X-API-KEY'] = os.getenv('API_KEY')
 
-        # Start
-        with openapi_client.ApiClient(configuration) as api_client:
-            self.api = openapi_client.DefaultApi(api_client)
-            self.start()
+        # Extra configuration
+        self._display_frame = self._read_int_from_env('DISPLAY_FRAME', 0) > 0
 
-    def start(self):
+        # Start client
+        with openapi_client.ApiClient(configuration) as api_client:
+            self._api = openapi_client.CafeteriaApi(api_client)
+            self._start()
+
+    def _start(self):
         """Start the system."""
         logging.info('System starts')
+        self._init_ingestion_stream()
 
-        last_batch_time = time.time()
+        self._last_batch_time = time.time()
         iteration = 0
-        while not self.should_close:
-            # Sleep to simulate computations
-            time.sleep(1)
+        while not self._should_close:
+            # Execute loop
+            self._execute_loop()
 
-            # Update counter
-            self.counter.update(time.time())
-            self.batcher.entered(self.counter.get_entering_list())
-            self.batcher.left(self.counter.get_leaving_list())
-
-            # Run batching
-            current_time = time.time()
-            if current_time > last_batch_time + BATCH_SECONDS:
-                last_batch_time = current_time
-
-                # Log data
-                batch = self.batcher.batch()
-                logging.debug('People in: {0}'.format(len(batch.entering)))
-                logging.debug('People out: {0}'.format(len(batch.leaving)))
+            # Get camera frame
+            if self._display_frame:
+                frame = self._frame_ingestor.get_frame()
+                cv2.imshow('Frame', frame)
+                cv2.waitKey(1)
 
             # Count iterations for demo purposes
             iteration += 1
             if iteration == 10:
-                self.should_close = True
+                self._should_close = True
 
         logging.info('System shutting down...')
+        self._finish_ingestion_stream()
 
-    def handle_signals(self, _signum, _frame):
+    def _init_ingestion_stream(self):
+        """Init ingestion stream."""
+        logging.info('Opening camera stream...')
+
+        stream_type = self._read_int_from_env('FRAME_SOURCE')
+        if stream_type is None:
+            self._should_close = True
+            return
+
+        if stream_type == 0:  # File
+            path = os.getenv('SOURCE_FILE_PATH', None)
+            source = FileIngestorSource(path)
+        elif stream_type == 1:  # Webcam
+            stream_num = self._read_int_from_env('WEBCAM_STREAM_NUM')
+            if stream_num is None:
+                self._should_close = True
+                return
+            source = WebcamIngestorSource(stream_num)
+
+        try:
+            self._frame_ingestor.register_source(source)
+        except RuntimeError as ex:
+            msg = 'Could not register_source: {0}'.format(ex)
+            logging.error(msg)
+            self._should_close = True
+
+    def _finish_ingestion_stream(self):
+        if self._display_frame:
+            cv2.destroyAllWindows()
+        if self._frame_ingestor.has_source():
+            logging.info('Closing ingestor source...')
+            self._frame_ingestor.release_source()
+
+    def _execute_loop(self):
+        """Execute main program loop."""
+        # Sleep to simulate computations
+        time.sleep(1)
+
+        # Update counter
+        self._counter.update(time.time())
+        self._batcher.entered(self._counter.get_entering_list())
+        self._batcher.left(self._counter.get_leaving_list())
+
+        # Run batching
+        current_time = time.time()
+        if current_time > self._last_batch_time + BATCH_SECONDS:
+            self._last_batch_time = current_time
+
+            # Log data
+            batch = self._batcher.batch()
+            logging.debug('People in: {0}'.format(len(batch.entering)))
+            logging.debug('People out: {0}'.format(len(batch.leaving)))
+
+    def _handle_signals(self, _signum, _frame):
         """Handle interruption events.
 
         Args:
             _signum: Signal number
             _frame: Current stack frame
         """
-        self.should_close = True
+        self._should_close = True
+
+    def _read_int_from_env(self, name, default=None):
+        """Read integer from environment.
+
+        Args:
+            name: variable name
+            default: variable default value
+
+        Returns:
+            Value or None on parsing error
+        """
+        try:
+            return int(os.getenv(name, default))
+        except TypeError as ex:
+            msg = 'Could not parse {0}: {1}'.format(name, ex)
+            logging.error(msg)
+            return None
 
 
 if __name__ == '__main__':
