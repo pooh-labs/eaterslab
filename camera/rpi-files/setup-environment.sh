df12217568e5373ef535c9ad07e1da6782a909ec
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-05-05 15:46:50 +0200

    Update dependencies

--- camera/rpi-files/setup-environment.sh
+++ camera/rpi-files/setup-environment.sh
@@ -11,11 +11,11 @@ fi
 
 # Install Python, OpenJDK and wget
 sudo apt-get update &&
-sudo apt-get install python3-dev python3-pip python3-setuptools python3-wheel openjdk-11-jdk wget &&
+sudo apt-get install python3-dev python3-pip python3-setuptools python3-wheel openjdk-11-jre wget &&
 
 # Set up virtual environment
 pip3 install --user pipenv &&
-pipenv install &&
+~/.local/bin/pipenv install &&
 
 # Download API generator
 wget "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_GENERATOR_VERSION}/openapi-generator-cli-${OPENAPI_GENERATOR_VERSION}.jar" -O "openapi-generator-cli.jar" &&
