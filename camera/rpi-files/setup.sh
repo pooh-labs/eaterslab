08d9eb9942ab2f0b71712f609d1a6963e389285e
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-05-05 21:34:44 +0200

    Fix camera and API workflows (#106)

--- camera/rpi-files/setup.sh
+++ camera/rpi-files/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-OPENAPI_GENERATOR_VERSION="4.2.3"
-
 # Force running from the same directory
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 if [ "$PWD" != "${SCRIPT_PATH}" ]; then
     echo >&2 "Error: Script called from a different directory" 
     exit 1
@@ -12,14 +11,5 @@ fi
 sudo systemctl enable ssh &&
 sudo systemctl start ssh &&
 
-# Install Python
-sudo apt-get update &&
-sudo apt-get install python3.7 openjdk-11-jdk &&
-
-# Set up virtual environment
-pip install pipenv &&
-pipenv install &&
-
-# Download API generator
-wget "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_GENERATOR_VERSION}/openapi-generator-cli-${OPENAPI_GENERATOR_VERSION}.jar" -O "openapi-generator-cli.jar"
-./generate-api-client.sh
+# Setup environment
+./setup-environment.sh
