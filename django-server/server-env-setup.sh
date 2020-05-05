163ecd3229ae0327e1c9a8f091352f273d79fba2
Maciej Procyk <macieekprocyk@gmail.com>
2020-05-05 21:16:13 +0200

    gh-pages clean

--- django-server/server-env-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script assumes you have python3, pip and pipenv installed
-# you can install pipenv using pip3 install pipenv
-echo "Starting installation of virtual environment"
-cd server
-pipenv install
-pipenv shell
-echo "Done"
-
--- django-server/server/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MenuOptionTag, Cafeteria, FixedMenuOption, FixedMenuOptionReview
-
-admin.site.register(Cafeteria)
-admin.site.register(FixedMenuOption)
-admin.site.register(FixedMenuOptionReview)
-admin.site.register(MenuOptionTag)
