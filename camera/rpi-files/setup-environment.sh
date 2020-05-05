0572d42b8d38ee8c27e9ede9f34484f93bfbffe1
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-05-05 16:15:06 +0200

    Update setup-environment.sh

--- camera/rpi-files/setup-environment.sh
+++ camera/rpi-files/setup-environment.sh
@@ -3,8 +3,6 @@
 OPENAPI_GENERATOR_VERSION="4.2.3"
 API_PATH="../../specs/api.yaml"
 
-PIPENV="~/.local/bin/pipenv"
-
 # Force running from the same directory
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 if [ "$PWD" != "${SCRIPT_PATH}" ]; then
@@ -15,8 +13,7 @@ fi
 # Install Python, OpenJDK and wget
 sudo apt-get update &&
 sudo apt-get install python3.7 python3-pip python3-setuptools python3-wheel openjdk-11-jre wget &&
-pip3 install --user pipenv &&
-alias pipenv="${PIPENV_PATH}" &&
+sudo pip3 install pipenv &&
 
 # Download API generator
 wget "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_GENERATOR_VERSION}/openapi-generator-cli-${OPENAPI_GENERATOR_VERSION}.jar" -O "openapi-generator-cli.jar" &&
@@ -26,4 +23,4 @@ rm -rf ./api-client/ &&
 java -jar openapi-generator-cli.jar generate -i "${API_PATH}" -g python -o api-client/ &&
 
 # Set up virtual environment
-${PIPENV} install
+pipenv install
