da22e018a37ee7c525315db6f170506595997656
Maciej Procyk <macieekprocyk@gmail.com>
2020-05-05 11:19:32 +0200

    repository planning

--- android/app/src/main/java/labs/pooh/eaterslab/repository/CafeteriaRepository.kt
+++ android/app/src/main/java/labs/pooh/eaterslab/repository/CafeteriaRepository.kt
@@ -1,13 +1,31 @@
 package labs.pooh.eaterslab.repository
 
+import labs.pooh.client.apis.CafeteriasApi
+import labs.pooh.client.infrastructure.ClientException
+import labs.pooh.client.infrastructure.ServerException
 import labs.pooh.eaterslab.repository.dao.CafeteriaDao
 import labs.pooh.eaterslab.repository.dao.FixedMenuOptionReviewDao
 import labs.pooh.eaterslab.repository.dao.MenuOptionTagDao
+import labs.pooh.eaterslab.repository.dao.toDao
 
-class CafeteriaRepository {
+object CafeteriaRepository {
+
+    private val cafeteriasApi by lazy { CafeteriasApi() }
+
     private val cachedCafeterias: Map<Int, CafeteriaDao> = mutableMapOf()
     private val cachedTags: Map<Int, MenuOptionTagDao> = mutableMapOf()
     private val cachedReviews: Map<Int, FixedMenuOptionReviewDao> = mutableMapOf()
 
+    fun cafeteriasList(): List<CafeteriaDao> {
+        try {
+            val data = cafeteriasApi.cafeteriasList()
+        } catch (e: ClientException) {
 
+        } catch (e: ServerException) {
+
+        } catch (e: UnsupportedOperationException) {
+
+        }
+        return listOf()
+    }
 }
