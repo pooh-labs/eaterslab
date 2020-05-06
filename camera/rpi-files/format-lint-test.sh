3ed72f5a830be5f58a982343374ea33ad3e34829
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-05-06 14:34:41 +0200

    Update testing suite

--- camera/rpi-files/format-lint-test.sh
+++ camera/rpi-files/format-lint-test.sh
@@ -26,5 +26,5 @@ ALL_TESTS=$(IFS=$' '; echo "${TESTS[*]}")
 
 isort ${ALL_FILES} &&
 black -t py37 -S -l 79 ${ALL_FILES} &&
-flake8 --config=setup.cfg ${ALL_FILES}
+flake8 --config=setup.cfg ${ALL_FILES} &&
 pytest ${ALL_TESTS}
