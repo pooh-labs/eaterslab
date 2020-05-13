3d25ec2bc903cacca93e8203e63de8017a3b2346
Maciej Procyk <macieekprocyk@gmail.com>
2020-05-13 17:29:05 +0200

    remove parsing time strings

--- android/app/src/main/java/labs/pooh/eaterslab/repository/dao/CafeteriaDao.kt
+++ android/app/src/main/java/labs/pooh/eaterslab/repository/dao/CafeteriaDao.kt
@@ -21,14 +21,4 @@ data class CafeteriaDao (
 
 fun Cafeteria.toDao() =
     CafeteriaDao(name, description, subDescription, longitude.toDouble(), latitude.toDouble(),
-        logoUrl, address, parseTime(openedFrom), parseTime(openedTo), id)
-
-private fun parseTime(stringTime: String): String {
-    return if (stringTime.count { it == ':' } > 1) {
-        val valid = stringTime.indexOf(':')
-        val index = stringTime.indexOf(':', startIndex = valid + 1)
-        stringTime.substring(0, index)
-    } else {
-        stringTime
-    }
-}
+        logoUrl, address, openedFrom, openedTo, id)
