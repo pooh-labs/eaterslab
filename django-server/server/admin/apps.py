7ef77d63c2d65e9fce5a9a7c684136d722b95e20
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-05-20 16:35:42 +0200

    Add new admin site

---
+++ django-server/server/admin/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AdminConfig(AppConfig):
+    name = 'admin'
+    label= 'myadmin'
+    default_site = 'admin.MyAdminSite'
+    
--- django-server/server/server/urls.py
+++ django-server/server/server/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from api.admin import admin_site
+from admin.admin import admin_site
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import permissions
