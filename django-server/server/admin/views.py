b045d4d754dcae5178ec9ab10cb05ebfb3b135ca
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-05-22 10:58:31 +0200

    Move `stats` template to admin

--- django-server/server/admin/views.py
+++ django-server/server/admin/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 
 class StatsView(TemplateView):
     admin_site = None
-    template_name = "stats.html"
+    template_name = "admin/stats.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**{
