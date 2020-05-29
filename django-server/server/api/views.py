8fb5b11218e95f13778631a0f6e20585525604fe
Maciej Procyk <macieekprocyk@gmail.com>
2020-05-29 20:22:15 +0200

    fixing people counting

--- django-server/server/api/views.py
+++ django-server/server/api/views.py
@@ -119,9 +119,6 @@ class StatsDivider:
     def get_timestamp_delta(self, time: datetime):
         pass
 
-    def get_timestamp_name(self, time: datetime):
-        return time.isoformat()
-
 
 class HourStatsDivider(StatsDivider):
     def get_timestamp_delta(self, time: datetime):
@@ -298,7 +295,7 @@ class OccupancyStatsView(StatsView):
         return OccupancyStatsData(id=index,
                                   timestamp=timestamp,
                                   occupancy=value,
-                                  occupancy_relative=(float(value) / float(capacity)))
+                                  occupancy_relative=min((float(value) / float(capacity)), 1.0))
 
 
 AVAILABLE_STATS = [
