81a28d6e3cc49c7c42506d9c95db115c0546f197
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-05-30 23:19:30 +0200

    Remove unused imports

--- django-server/server/api/models.py
+++ django-server/server/api/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator, URLValidator
-from django.db.models import F, Q, Case, When, Value
+from django.db.models import F, Q
 
 from datetime import timedelta
 
