a862573ff51ffb80c26d9db3d595b3534a4cb4d2
Maciej Procyk <macieekprocyk@gmail.com>
2020-06-06 01:54:54 +0200

    live data update visible on client

--- android/app/src/main/java/labs/pooh/eaterslab/ui/fragment/home/CafeteriaFragment.kt
+++ android/app/src/main/java/labs/pooh/eaterslab/ui/fragment/home/CafeteriaFragment.kt
@@ -21,10 +21,14 @@ class CafeteriaFragment : ThemedAbstractFragment() {
         }).get(CafeteriaViewModel::class.java)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.runPeriodicDataUpdate()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- android/app/src/main/java/labs/pooh/eaterslab/ui/fragment/home/CafeteriaViewModel.kt
+++ android/app/src/main/java/labs/pooh/eaterslab/ui/fragment/home/CafeteriaViewModel.kt
@@ -4,13 +4,17 @@ import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import labs.pooh.eaterslab.repository.dao.CafeteriaDao
 import labs.pooh.eaterslab.ui.activity.abstracts.ConnectionStatusNotifier
 import labs.pooh.eaterslab.ui.activity.abstracts.RepositoryAccessViewModel
 import labs.pooh.eaterslab.ui.activity.main.MainActivity
 
 class CafeteriaViewModel(notifier: ConnectionStatusNotifier) : RepositoryAccessViewModel(notifier) {
+
+    companion object {
+        const val OCCUPANCY_DATA_UPDATE_MILLIS = 1_000
+    }
 
     private val _cafeteriaName = MutableLiveData<String>()
     val cafeteriaName: LiveData<String> = _cafeteriaName
@@ -50,6 +54,17 @@ class CafeteriaViewModel(notifier: ConnectionStatusNotifier) : RepositoryAccessV
         }
     }
 
+    fun runPeriodicDataUpdate() = viewModelScope.launch {
+        while (this.isActive) {
+            val cafeteria = MainActivity.lastSelectedCafeteriaId
+            val updatedData = repository.cafeteriasRead(cafeteria)
+            updatedData?.occupancy?.let {
+                _cafeteriaOccupancy.value = scaleOccupancy(it)
+            }
+            delay(OCCUPANCY_DATA_UPDATE_MILLIS.toLong())
+        }
+    }
+
     private suspend fun updateTextCafeteriaData() {
         val cafeteriaId = MainActivity.lastSelectedCafeteriaId
         val cafeteria = repository.cafeteriasRead(cafeteriaId)
@@ -61,7 +76,9 @@ class CafeteriaViewModel(notifier: ConnectionStatusNotifier) : RepositoryAccessV
             _cafeteriaOpenFrom.value = openedFrom
             _cafeteriaOpenTo.value = openedTo
             _cafeteriaAddress.value = address
-            _cafeteriaOccupancy.value = (occupancy * 100).toInt()
+            _cafeteriaOccupancy.value = scaleOccupancy(occupancy)
         }
     }
+
+    private fun scaleOccupancy(modelData: Double) = (modelData * 100).toInt()
 }
